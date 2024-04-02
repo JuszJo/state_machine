8b19a08e5fa3dfdf75c37d3c3957a5f19deb4580
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2024-04-02 20:00:24 +0100

    clean up

--- game.h
+++ game.h
@@ -1,12 +1,10 @@
 #pragma once
 
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
 
 #include "utils/my_array.h"
-#include "src/entityV2.h"
 
-// #include "src/keyinput.h"
+#include "src/entityV2.h"
 
 class Game {
     public:
@@ -14,8 +12,6 @@ class Game {
         int* display_w;
         int *display_h;
         glm::mat4 projection;
-
-        // static KeyInput keyInput;
 
         Game(int* display_w, int* display_h);
 
