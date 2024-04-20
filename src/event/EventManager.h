a62b18de96ed7e8b4327630b417f3104374496b6
Joshua Ubani-Wokoma <joshubaniwokoma@gmail.com>
2024-04-20 15:33:28 +0100

    add event class

--- src/event/EventManager.h
+++ src/event/EventManager.h
@@ -2,8 +2,14 @@
 
 #include <vector>
 
+class Event {
+    public:
+};
+
 class EventManager {
     public:
-
+        static const std::vector<Event> events;
+        
+        EventManager() {};
 };
 
